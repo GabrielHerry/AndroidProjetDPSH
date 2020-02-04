1f8fc83cdcb35a3b277136f0a49f5f304b95b302
GabrielHerry <gabriel.herry.fr@gmail.com>
2020-02-04 16:01:26 +0100

    GH Branch -- ip pass in showpostact ok !

--- app/src/main/java/com/isen/androidprojetdpsh/ShowPostActivity.kt
+++ app/src/main/java/com/isen/androidprojetdpsh/ShowPostActivity.kt
@@ -8,5 +8,7 @@ class ShowPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_post)
+
+        var currentpost = intent.getIntExtra("postId",0)
     }
 }
