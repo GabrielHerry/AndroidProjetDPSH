9095b90e296e29f2386a8df7f6c1df52b9ce4595
SourceTree_Peltier <peltier.lucile.3@gmail.com>
2020-02-04 16:33:31 +0100

    Login + register new account

--- app/src/main/java/com/isen/androidprojetdpsh/MainActivity.kt
+++ app/src/main/java/com/isen/androidprojetdpsh/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.isen.androidprojetdpsh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        signOutButton.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        auth?.signOut()
+        Toast.makeText(this, "${getString(R.string.log_out)}", Toast.LENGTH_LONG).show()
+        val intent = Intent( this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
     }
 }
